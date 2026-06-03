9f162afee38b278780ce40c2cb3d4216e07c8d4a
Aaron Evanjulio <aaronevanjulio18@gmail.com>
2026-06-03 10:31:34 +0700

    fix: ensure navigation bar background is visible in toolbar extension

--- Sources/OwlNav/Extensions/Views+Extensions.swift
+++ Sources/OwlNav/Extensions/Views+Extensions.swift
@@ -21,6 +21,7 @@ extension View {
             } else {
                 self
                     .toolbar(.visible, for: .navigationBar)
+                    .toolbarBackground(.visible, for: .navigationBar)
             }
         } else {
             self
