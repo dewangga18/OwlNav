9c28e3911ed7fbc6dd7744d7d647b06483dc0bc9
Aaron Evanjulio <aaronevanjulio18@gmail.com>
2026-06-03 10:09:38 +0700

    fix: revert from initial setup

--- Sources/OwlNav/Setup/OwlInject.swift
+++ Sources/OwlNav/Setup/OwlInject.swift
@@ -8,15 +8,10 @@
 #if canImport(UIKit)
 import UIKit
 
-public enum OwlNavBarBehavior {
-    case alwaysHidden
-    case transparentWhenAtTop
-}
-
 /// A utility struct to inject global navigation styles.
 @MainActor public struct OwlInject {
     /// Configures the global appearance of the navigation bar to be transparent and hides the back button.
-    public static func initFunc(navBarBehavior: OwlNavBarBehavior = .transparentWhenAtTop) {
+    public static func initFunc() {
         let backButtonAppearance = UIBarButtonItemAppearance()
         backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.clear]
         backButtonAppearance.highlighted.titleTextAttributes = [.foregroundColor: UIColor.clear]
@@ -32,21 +27,8 @@ public enum OwlNavBarBehavior {
         appearance.tintColor = .clear
         appearance.isHidden = true
 
-        switch navBarBehavior {
-        case .alwaysHidden:
-            appearance.scrollEdgeAppearance = navAppearance
-            if #available(iOS 15.0, *) {
-                appearance.compactScrollEdgeAppearance = navAppearance
-            }
-        case .transparentWhenAtTop:
-            let transparentAppearance = UINavigationBarAppearance()
-            transparentAppearance.configureWithTransparentBackground()
-            transparentAppearance.backButtonAppearance = backButtonAppearance
-            transparentAppearance.setBackIndicatorImage(UIImage(), transitionMaskImage: UIImage())
-            appearance.scrollEdgeAppearance = transparentAppearance
-            if #available(iOS 15.0, *) {
-                appearance.compactScrollEdgeAppearance = transparentAppearance
-            }
+        if #available(iOS 15.0, *) {
+            appearance.compactScrollEdgeAppearance = navAppearance
         }
     }
 }
